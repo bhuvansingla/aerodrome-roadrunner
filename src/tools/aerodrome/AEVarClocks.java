b03fc55648ef17ec95250dd669dfcf2688850524
Krishanu Singh <krishanu230@hotmail.com>
2021-04-20 22:31:56 +0530

    tempMerge-Krishanu

---
+++ src/tools/aerodrome/AEVarClocks.java
@@ -0,0 +1,103 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2016, Cormac Flanagan (University of California, Santa Cruz) and Stephen Freund
+ * (Williams College)
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ * and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the names of the University of California, Santa Cruz and Williams College nor the names
+ * of its contributors may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************/
+
+package tools.aerodrome;
+
+import rr.state.ShadowVar;
+import tools.util.Epoch;
+import tools.util.VectorClock;
+
+public class AEVarState implements ShadowVar {
+  /******************************************************************************
+   *
+   * Copyright (c) 2016, Cormac Flanagan (University of California, Santa Cruz) and Stephen Freund
+   * (Williams College)
+   *
+   * All rights reserved.
+   *
+   * Redistribution and use in source and binary forms, with or without modification, are permitted
+   * provided that the following conditions are met:
+   *
+   * Redistributions of source code must retain the above copyright notice, this list of conditions
+   * and the following disclaimer.
+   *
+   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+   * and the following disclaimer in the documentation and/or other materials provided with the
+   * distribution.
+   *
+   * Neither the names of the University of California, Santa Cruz and Williams College nor the names
+   * of its contributors may be used to endorse or promote products derived from this software without
+   * specific prior written permission.
+   *
+   * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+   * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+   * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+   * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+   * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+   * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   *
+   ******************************************************************************/
+
+  package tools.util;
+
+  import acme.util.count.Counter;
+  import rr.state.ShadowVar;
+
+  public class AEVarClocks implements ShadowVar {
+      public final VectorClock rd;
+      public final VectorClock wr;
+      public final VectorClock rdchk;
+
+
+      private static Counter AEVarClocks = new Counter("AEVarClocks", "triplet Objects");
+
+      public AEVarClocks(VectorClock vc) {
+          rd = new VectorClock(vc);
+          wr = new VectorClock(vc);
+          rdchk = new VectorClock(vc);
+          AEVarClocks.inc();
+      }
+
+      public AEVarClocks() {
+          rd = new VectorClock(1);
+          wr = new VectorClock(1);
+          rdchk = new VectorClock(1);
+          AEVarClocks.inc();
+      }
+
+      @Override
+      public String toString() {
+          return "R" + rd + " - " + "W" + wr + "RCHK"+rdchk;
+      }
+  }
